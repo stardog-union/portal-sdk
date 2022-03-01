462e819d6126dc8be8844a64d258413adec9b42c
SpiralP <spiral.palmer@gmail.com>
2022-03-01 14:39:23 -0800

    add test for `getCurrentConnectionInfo`

---
+++ src/cookies/__tests__/getCurrentPortalUrlInfo.test.ts
@@ -0,0 +1,35 @@
+import { getCurrentConnectionInfo } from '../../getCurrentConnectionInfo';
+
+const windowFixture = {
+  location: {
+    hash: '#/',
+    host: 'localhost:8080',
+    hostname: 'localhost',
+    href: 'http://localhost:8080/u/0/explorer/#/',
+    origin: 'http://localhost:8080',
+    pathname: '/u/0/explorer/',
+    port: '8080',
+    protocol: 'http:',
+  },
+} as any as Window;
+
+describe('getCurrentConnectionInfo', () => {
+  it('returns connectionIndex and product of current valid url', () => {
+    const window = { ...windowFixture };
+
+    const info = getCurrentConnectionInfo(window);
+    expect(info).toBeTruthy();
+    expect(info?.connectionIndex).toBe(0);
+    expect(info?.product).toBe('explorer');
+  });
+
+  it('returns null for invalid urls', () => {
+    const window = { ...windowFixture };
+
+    expect(getCurrentConnectionInfo(window)).toBeTruthy();
+
+    window.location.href = 'http://localhost:8080/u/0/';
+    window.location.pathname = '/u/0/';
+    expect(getCurrentConnectionInfo(window)).toBe(null);
+  });
+});
