3ac43dbc5f31fc640aa0c74aa398b4960f9d8553
SpiralP <spiral.palmer@gmail.com>
2022-03-01 15:13:56 -0800

    update tests

--- src/__tests__/getCurrentPortalUrlInfo.test.ts
+++ src/__tests__/getCurrentPortalUrlInfo.test.ts
@@ -5,9 +5,9 @@ const windowFixture = {
     hash: '#/',
     host: 'localhost:8080',
     hostname: 'localhost',
-    href: 'http://localhost:8080/u/0/explorer/#/',
+    href: 'http://localhost:8080/u/9/explorer/#/',
     origin: 'http://localhost:8080',
-    pathname: '/u/0/explorer/',
+    pathname: '/u/9/explorer/',
     port: '8080',
     protocol: 'http:',
   },
@@ -19,17 +19,30 @@ describe('getCurrentConnectionInfo', () => {
 
     const info = getCurrentConnectionInfo(window);
     expect(info).toBeTruthy();
-    expect(info?.connectionIndex).toBe(0);
+    expect(info?.connectionIndex).toBe(9);
     expect(info?.product).toBe('explorer');
   });
 
-  it('returns null for invalid urls', () => {
+  it('returns correct info for missing-product url', () => {
     const window = { ...windowFixture };
 
     expect(getCurrentConnectionInfo(window)).toBeTruthy();
 
-    window.location.href = 'http://localhost:8080/u/0/';
-    window.location.pathname = '/u/0/';
+    window.location.href = 'http://localhost:8080/u/7/';
+    window.location.pathname = '/u/7/';
+    const info = getCurrentConnectionInfo(window);
+    expect(info).toBeTruthy();
+    expect(info?.connectionIndex).toBe(7);
+    expect(info?.product).toBe(undefined);
+  });
+
+  it('returns null for invalid paths', () => {
+    const window = { ...windowFixture };
+
+    expect(getCurrentConnectionInfo(window)).toBeTruthy();
+
+    window.location.href = 'http://localhost:8080/invalid/u/0/';
+    window.location.pathname = '/invalid/u/0/';
     expect(getCurrentConnectionInfo(window)).toBe(null);
   });
 });
