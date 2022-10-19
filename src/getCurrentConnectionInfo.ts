1c6d3cd80ff099af7ac552b2823f7ccd54fa20a3
SpiralP <spiral.palmer@gmail.com>
2022-10-18 18:32:12 -0700

    [VET-2602] Update helper in portal-sdk to give productPath
    * let slash after connection index be optional
    * add `productPath` to getCurrentConnectionInfo

--- src/getCurrentConnectionInfo.ts
+++ src/getCurrentConnectionInfo.ts
@@ -1,22 +1,41 @@
+/**
+ * Information parsed out of the current url.
+ *
+ * `/u/<connectionIndex>/[product]/[productPath...]`
+ */
 export type ConnectionInfo = {
   connectionIndex: number;
 
   /** an optional string like "designer" | "explorer" | "studio" */
   product?: string;
+
+  /** an optional string of the path to the right of `product`
+   * (not including the first slash)
+   * such as "unstable/index.html" | "snapshot/index.html" */
+  productPath?: string;
 };
 
 /** returns info on the current Portal/Cloud connection */
 export const getCurrentConnectionInfo = (
   window: Window
 ): ConnectionInfo | null => {
-  const urlPathRegex = /^\/u\/(?<connectionIndex>[0-9]+)\/(?<product>[^\/]+)?/;
-  const currentMatch = window.location.pathname.match(urlPathRegex);
-  if (currentMatch?.groups) {
-    return {
-      connectionIndex: Number.parseInt(currentMatch.groups.connectionIndex),
-      product: currentMatch.groups.product as string | undefined,
-    };
+  const connectionIndexMatch = window.location.pathname.match(
+    /^\/u\/([0-9]+)(?:\/(.*))?$/
+  );
+  if (!connectionIndexMatch) {
+    return null;
   }
 
-  return null;
+  const connectionIndex = Number.parseInt(connectionIndexMatch[1]);
+  const connectionIndexExtra = connectionIndexMatch[2];
+
+  const productMatch = connectionIndexExtra?.match(/^([^\/]+)(?:\/(.*))?$/);
+  const product = productMatch?.[1] || undefined;
+  const productPath = productMatch?.[2] || undefined;
+
+  return {
+    connectionIndex,
+    product,
+    productPath,
+  };
 };
