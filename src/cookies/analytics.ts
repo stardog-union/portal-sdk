bff72c32c8f9cd16124f34297f0f5e87c78253c1
Rob Boskind <49008703+mysticfalconvt@users.noreply.github.com>
2024-03-13 10:55:41 -0400

    Revert "Use existing consent and then delete existing consent"
    This reverts commit cc4a2e9c5d704988b36faab484e899850d4d4e3b.

--- src/cookies/analytics.ts
+++ src/cookies/analytics.ts
@@ -1,10 +1,6 @@
 /// <reference types="cookiebot-sdk" />
 
-import { getCookieValue } from './utils/getCookieValue';
-import { optionalTypeCheck } from './utils/optionalTypeCheck';
-
 export const ANALYTICS_COOKIE_NAME = 'CookieConsent';
-export const DEPRECATED_ANALYTICS_COOKIE_NAME = 'stardogAnalyticsConsent';
 
 export type AnalyticsConsent = {
   marketing: boolean;
@@ -13,24 +9,7 @@ export type AnalyticsConsent = {
   statistics: boolean;
 };
 
-export type DeprecatedAnalyticsConsent = {
-  consented: boolean;
-  identity?: string;
-};
-
 export const getAnalyticsConsent = (): AnalyticsConsent | null => {
-  const deprecatedCookie = getDeprecatedAnalyticsConsentCookie();
-  if (deprecatedCookie) {
-    const consented = deprecatedCookie.consented;
-    // delete the deprecated cookie
-    document.cookie = `${DEPRECATED_ANALYTICS_COOKIE_NAME}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/;`;
-    return {
-      marketing: consented,
-      necessary: consented,
-      preferences: consented,
-      statistics: consented,
-    };
-  }
   if (!window.Cookiebot || !window.Cookiebot.hasResponse) {
     return null;
   }
@@ -42,36 +21,3 @@ export const getAnalyticsConsent = (): AnalyticsConsent | null => {
     preferences: window.Cookiebot.consent.preferences,
   };
 };
-
-export const decodeAnalyticsCookie = (
-  rawConsentCookie: string
-): DeprecatedAnalyticsConsent | null => {
-  try {
-    const decoded = JSON.parse(atob(rawConsentCookie));
-    const consented: boolean = decoded.consented;
-    const identity: string = decoded.identity;
-
-    if (
-      typeof consented !== 'boolean' ||
-      !optionalTypeCheck(identity, 'string')
-    ) {
-      throw new Error('invalid cookie');
-    }
-
-    return {
-      consented,
-      identity,
-    };
-  } catch {
-    return null;
-  }
-};
-
-export const getDeprecatedAnalyticsConsentCookie =
-  (): DeprecatedAnalyticsConsent | null => {
-    const rawConsentCookie = getCookieValue(DEPRECATED_ANALYTICS_COOKIE_NAME);
-    if (!rawConsentCookie) {
-      return null;
-    }
-    return decodeAnalyticsCookie(rawConsentCookie);
-  };
